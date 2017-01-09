12596dc6955631158ce9a28a5027783d29967e2d
amiyapatanaik <amiyain@gmail.com>
2017-01-09 15:18:58 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pycfslib',
+      version='0.1',
+      description='Library to read, write amd create CFS file and stream.',
+      url='http://github.com/amiyapatanaik/pycfslib',
+      author='Amiya Patanaik',
+      author_email='amiya@duke-nus.edu.sg',
+      license='GPL',
+      packages=['pycfslib'],
+      zip_safe=False)
