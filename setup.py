37d2763979db77c108c4c1a1adc25c816c400302
Neurobit Technologies <admin@neurobit.io>
2018-12-22 21:36:06 +0800

    updated to v1.0

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup
 
 setup(name='pycfslib',
-      version='0.8',
+      version='1.0',
       description='Library to read, write amd create CFS file and stream, now supports NEO sleep staging system.',
       url='https://github.com/neurobittechnologies/pycfslib',
       author='Amiya Patanaik',
       author_email='amiya@neurobit.io',
       license='GPL',
       packages=['pycfslib'],
+      install_requires=[
+          'numpy',
+          'scikit-image',
+          'scipy',
+          'numba',
+      ],
       zip_safe=False)
